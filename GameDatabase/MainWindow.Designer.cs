3f24c4f8253772ca4c32412c60d8f49f6e8f018b
feelsthebeats1 <clouder14526363@gmail.com>
2026-01-31 18:44:58 +0700

    Hmmm

--- GameDatabase/MainWindow.Designer.cs
+++ GameDatabase/MainWindow.Designer.cs
@@ -30,194 +30,195 @@ namespace GameDatabase
         /// </summary>
         private void InitializeComponent()
         {
-            this.DatabaseTreeView = new System.Windows.Forms.TreeView();
-            this.splitContainer = new System.Windows.Forms.SplitContainer();
-            this.structDataView1 = new GameDatabase.Controls.StructDataView();
-            this.EditButton = new System.Windows.Forms.Button();
-            this.ItemTypeText = new System.Windows.Forms.Label();
-            this.ItemTypeLabel = new System.Windows.Forms.Label();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.createModMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
-            this.splitContainer.Panel1.SuspendLayout();
-            this.splitContainer.Panel2.SuspendLayout();
-            this.splitContainer.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
+            DatabaseEditor.Properties.Settings settings1 = new DatabaseEditor.Properties.Settings();
+            DatabaseTreeView = new TreeView();
+            splitContainer = new SplitContainer();
+            structDataView1 = new StructDataView();
+            EditButton = new Button();
+            ItemTypeText = new Label();
+            ItemTypeLabel = new Label();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            loadMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsMenuItem = new ToolStripMenuItem();
+            createModMenuItem = new ToolStripMenuItem();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            saveFileDialog = new SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // DatabaseTreeView
             // 
-            this.DatabaseTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DatabaseTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DatabaseTreeView.Location = new System.Drawing.Point(6, 3);
-            this.DatabaseTreeView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.DatabaseTreeView.Name = "DatabaseTreeView";
-            this.DatabaseTreeView.Size = new System.Drawing.Size(383, 568);
-            this.DatabaseTreeView.TabIndex = 0;
-            this.DatabaseTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.DatabaseTreeView_AfterSelect);
-            this.DatabaseTreeView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.DatabaseTreeView_MouseDoubleClick);
+            DatabaseTreeView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DatabaseTreeView.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            DatabaseTreeView.Location = new Point(6, 3);
+            DatabaseTreeView.Margin = new Padding(4, 5, 4, 5);
+            DatabaseTreeView.Name = "DatabaseTreeView";
+            DatabaseTreeView.Size = new Size(383, 568);
+            DatabaseTreeView.TabIndex = 0;
+            DatabaseTreeView.AfterSelect += DatabaseTreeView_AfterSelect;
+            DatabaseTreeView.MouseDoubleClick += DatabaseTreeView_MouseDoubleClick;
             // 
             // splitContainer
             // 
-            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer.Location = new System.Drawing.Point(0, 24);
-            this.splitContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.splitContainer.Name = "splitContainer";
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Location = new Point(0, 24);
+            splitContainer.Margin = new Padding(4, 5, 4, 5);
+            splitContainer.Name = "splitContainer";
             // 
             // splitContainer.Panel1
             // 
-            this.splitContainer.Panel1.Controls.Add(this.DatabaseTreeView);
+            splitContainer.Panel1.Controls.Add(DatabaseTreeView);
             // 
             // splitContainer.Panel2
             // 
-            this.splitContainer.Panel2.Controls.Add(this.structDataView1);
-            this.splitContainer.Panel2.Controls.Add(this.EditButton);
-            this.splitContainer.Panel2.Controls.Add(this.ItemTypeText);
-            this.splitContainer.Panel2.Controls.Add(this.ItemTypeLabel);
-            this.splitContainer.Size = new System.Drawing.Size(800, 576);
-            this.splitContainer.SplitterDistance = 393;
-            this.splitContainer.SplitterWidth = 6;
-            this.splitContainer.TabIndex = 1;
+            splitContainer.Panel2.Controls.Add(structDataView1);
+            splitContainer.Panel2.Controls.Add(EditButton);
+            splitContainer.Panel2.Controls.Add(ItemTypeText);
+            splitContainer.Panel2.Controls.Add(ItemTypeLabel);
+            splitContainer.Size = new Size(800, 576);
+            splitContainer.SplitterDistance = 393;
+            splitContainer.SplitterWidth = 6;
+            splitContainer.TabIndex = 1;
             // 
             // structDataView1
             // 
-            this.structDataView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.structDataView1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.structDataView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.structDataView1.Data = null;
-            this.structDataView1.Database = null;
-            this.structDataView1.ForeColor = System.Drawing.Color.DimGray;
-            this.structDataView1.Location = new System.Drawing.Point(4, 41);
-            this.structDataView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.structDataView1.Name = "structDataView1";
-            this.structDataView1.Size = new System.Drawing.Size(381, 483);
-            this.structDataView1.TabIndex = 5;
+            structDataView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            structDataView1.BackColor = Color.WhiteSmoke;
+            structDataView1.BorderStyle = BorderStyle.FixedSingle;
+            structDataView1.Data = null;
+            structDataView1.Database = null;
+            structDataView1.ForeColor = Color.DimGray;
+            structDataView1.Location = new Point(8, 41);
+            structDataView1.Margin = new Padding(4, 5, 4, 5);
+            structDataView1.Name = "structDataView1";
+            structDataView1.Size = new Size(379, 483);
+            structDataView1.TabIndex = 5;
             // 
             // EditButton
             // 
-            this.EditButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.EditButton.Enabled = false;
-            this.EditButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EditButton.Location = new System.Drawing.Point(8, 534);
-            this.EditButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(112, 35);
-            this.EditButton.TabIndex = 4;
-            this.EditButton.TabStop = false;
-            this.EditButton.Text = "Edit";
-            this.EditButton.UseVisualStyleBackColor = true;
-            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            EditButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            EditButton.Enabled = false;
+            EditButton.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            EditButton.Location = new Point(8, 534);
+            EditButton.Margin = new Padding(4, 5, 4, 5);
+            EditButton.Name = "EditButton";
+            EditButton.Size = new Size(112, 35);
+            EditButton.TabIndex = 4;
+            EditButton.TabStop = false;
+            EditButton.Text = "Edit";
+            EditButton.UseVisualStyleBackColor = true;
+            EditButton.Click += EditButton_Click;
             // 
             // ItemTypeText
             // 
-            this.ItemTypeText.AutoSize = true;
-            this.ItemTypeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ItemTypeText.Location = new System.Drawing.Point(90, 3);
-            this.ItemTypeText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.ItemTypeText.Name = "ItemTypeText";
-            this.ItemTypeText.Size = new System.Drawing.Size(14, 20);
-            this.ItemTypeText.TabIndex = 3;
-            this.ItemTypeText.Text = "-";
+            ItemTypeText.AutoSize = true;
+            ItemTypeText.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ItemTypeText.Location = new Point(90, 3);
+            ItemTypeText.Margin = new Padding(4, 0, 4, 0);
+            ItemTypeText.Name = "ItemTypeText";
+            ItemTypeText.Size = new Size(14, 20);
+            ItemTypeText.TabIndex = 3;
+            ItemTypeText.Text = "-";
             // 
             // ItemTypeLabel
             // 
-            this.ItemTypeLabel.AutoSize = true;
-            this.ItemTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ItemTypeLabel.Location = new System.Drawing.Point(3, 3);
-            this.ItemTypeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.ItemTypeLabel.Name = "ItemTypeLabel";
-            this.ItemTypeLabel.Size = new System.Drawing.Size(79, 20);
-            this.ItemTypeLabel.TabIndex = 2;
-            this.ItemTypeLabel.Text = "Item type:";
+            ItemTypeLabel.AutoSize = true;
+            ItemTypeLabel.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ItemTypeLabel.Location = new Point(3, 3);
+            ItemTypeLabel.Margin = new Padding(4, 0, 4, 0);
+            ItemTypeLabel.Name = "ItemTypeLabel";
+            ItemTypeLabel.Size = new Size(79, 20);
+            ItemTypeLabel.TabIndex = 2;
+            ItemTypeLabel.Text = "Item type:";
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Text = "menuStrip1";
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.loadMenuItem,
-            this.saveToolStripMenuItem,
-            this.saveAsMenuItem,
-            this.createModMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loadMenuItem, saveToolStripMenuItem, saveAsMenuItem, createModMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
             // 
             // loadMenuItem
             // 
-            this.loadMenuItem.Name = "loadMenuItem";
-            this.loadMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.loadMenuItem.Text = "Load";
-            this.loadMenuItem.Click += new System.EventHandler(this.loadMenuItem_Click);
+            loadMenuItem.Name = "loadMenuItem";
+            loadMenuItem.Size = new Size(145, 22);
+            loadMenuItem.Text = "Load";
+            loadMenuItem.Click += loadMenuItem_Click;
             // 
             // saveToolStripMenuItem
             // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(145, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
             // 
             // saveAsMenuItem
             // 
-            this.saveAsMenuItem.Name = "saveAsMenuItem";
-            this.saveAsMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveAsMenuItem.Text = "Save As...";
-            this.saveAsMenuItem.Click += new System.EventHandler(this.saveAsMenuItem_Click);
+            saveAsMenuItem.Name = "saveAsMenuItem";
+            saveAsMenuItem.Size = new Size(145, 22);
+            saveAsMenuItem.Text = "Save As...";
+            saveAsMenuItem.Click += saveAsMenuItem_Click;
             // 
             // createModMenuItem
             // 
-            this.createModMenuItem.Name = "createModMenuItem";
-            this.createModMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.createModMenuItem.Text = "Create Mod...";
-            this.createModMenuItem.Click += new System.EventHandler(this.createModMenuItem_Click);
+            createModMenuItem.Name = "createModMenuItem";
+            createModMenuItem.Size = new Size(145, 22);
+            createModMenuItem.Text = "Create Mod...";
+            createModMenuItem.Click += createModMenuItem_Click;
             // 
             // saveFileDialog
             // 
-            this.saveFileDialog.AddExtension = false;
-            this.saveFileDialog.FileName = "mod";
+            saveFileDialog.AddExtension = false;
+            saveFileDialog.FileName = "mod";
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = global::DatabaseEditor.Properties.Settings.Default.MainSize;
-            this.Controls.Add(this.splitContainer);
-            this.Controls.Add(this.menuStrip1);
-            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::DatabaseEditor.Properties.Settings.Default, "MainPosition", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.DataBindings.Add(new System.Windows.Forms.Binding("ClientSize", global::DatabaseEditor.Properties.Settings.Default, "MainSize", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Location = global::DatabaseEditor.Properties.Settings.Default.MainPosition;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "MainWindow";
-            this.Text = "Game database";
-            this.Load += new System.EventHandler(this.MainWindow_Load);
-            this.splitContainer.Panel1.ResumeLayout(false);
-            this.splitContainer.Panel2.ResumeLayout(false);
-            this.splitContainer.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
-            this.splitContainer.ResumeLayout(false);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 600);
+            Controls.Add(splitContainer);
+            Controls.Add(menuStrip1);
+            settings1.EditorPosition = new Point(100, 100);
+            settings1.EditorSize = new Size(400, 400);
+            settings1.MainPosition = new Point(100, 100);
+            settings1.MainSize = new Size(800, 600);
+            settings1.SettingsKey = "";
+            settings1.ShipEditorHorizontalSplitter = 272;
+            settings1.ShipEditorPosition = new Point(100, 100);
+            settings1.ShipEditorSize = new Size(800, 600);
+            settings1.ShipEditorVerticalSplitter = 364;
+            DataBindings.Add(new Binding("Location", settings1, "MainPosition", true, DataSourceUpdateMode.OnPropertyChanged));
+            DataBindings.Add(new Binding("ClientSize", settings1, "MainSize", true, DataSourceUpdateMode.OnPropertyChanged));
+            Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Location = new Point(100, 100);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "MainWindow";
+            Text = "Game database";
+            Load += MainWindow_Load;
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel2.ResumeLayout(false);
+            splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
